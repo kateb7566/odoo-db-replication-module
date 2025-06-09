838b84eedee86b6813f7e2aa10ed35adf3aad036
Fateh Lebbier <muhamet7138@gmail.com>
2025-06-09 11:51:39 +0100

    app folder restructure. written the manifest file.

---
+++ app/__manifest__.py
@@ -0,0 +1,25 @@
+# __manifest__.py
+
+{
+    "name": "Database Replication",
+    "version": "1.0.0",
+    "summary": "Module to manage database replication within Odoo instances.",
+    "description": """
+Database Replication Module
+===========================
+
+This module provides tools to configure and manage database replication setups across Odoo environments.
+It is designed for advanced users and system administrators needing better control over their Odoo deployments.
+    """,
+    "author": "Fateh Lebbier",
+    "website": "https://katebworks.com",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": [
+        # "views/replication_settings_view.xml",
+        # "security/ir.model.access.csv"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
